735ee920739f1baede72e18a6296c757575605a7
Zhulfaliant01 <zhulfaliant@gmail.com>
2024-03-23 04:59:24 +0700

    Update download_buildings.py
    - Menambahkan script untuk mengunduh data kecamatan yang belum lengkap, yaitu Jagakarsa, Mampang Prapatan, dan Pesanggrahan
    - Data yang diunduh sebelumnya belum lengkap, karena terdapat nama desa yang sama dengan nama kecamatan sehingga mengganggu proses pengunduhna dari OSMnx

--- Prepare the Dataset/download_buildings.py
+++ Prepare the Dataset/download_buildings.py
@@ -62,15 +62,201 @@ def download_building(place):
             print(f"Failed to export {kec}: {e}")
 
 
-def main():
+# Main function
+# def main():
+#     with ThreadPoolExecutor(max_workers=4) as executor:
+#         futures = [
+#             executor.submit(download_building, f"{kec}, {city}") for kec in kecamatan
+#         ]
+#         results = []
+#         for done in as_completed(futures):
+#             results.append(done)
+
+
+def download_buildingMampang(place):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        kec = place.replace(", Mampang Prapatan, Jakarta Selatan, Indonesia", "")
+        print(f"{kec} Start!")
+        try:
+            buildings = osm.features_from_place(
+                place, {"building": True, "type": "polygon"}
+            )
+            print(f"Successfully downloaded {kec}")
+        except Exception as e:
+            print(f"Failed to download {kec}: {e}")
+            return
+
+        # Ensure directory exists
+        output_dir = r"C:\Users\Lenovo\OneDrive - UGM 365\Documents\Second Brain\1 Projects\Skripsi\Code\Residential-Classification\Prepare the Dataset\building"  # Adjusted to use forward slashes
+        os.makedirs(
+            output_dir, exist_ok=True
+        )  # Create the directory if it doesn't exist
+
+        # Sanitize file name
+        file_name = place.replace(", Mampang Prapatan, Jakarta Selatan, Indonesia", "")
+        file_path = os.path.join(output_dir, f"building_desa_{file_name}.geojson")
+
+        try:
+            buildings = (
+                buildings[
+                    (buildings.geom_type == "Polygon")
+                    | (buildings.geom_type == "MultiPolygon")
+                ]
+                .reset_index(drop=True)
+                .drop(columns="ways")
+            )
+            for column in buildings.columns:
+                if isinstance(buildings[column].iloc[0], list):
+                    buildings[column] = buildings[column].apply(
+                        lambda x: ", ".join(map(str, x)) if isinstance(x, list) else x
+                    )
+
+            buildings.to_file(file_path, driver="GeoJSON")
+            print(f"Successfully exported to {file_path}")
+        except Exception as e:
+            print(f"Failed to export {kec}: {e}")
+
+
+# Khusus Mampang Prapatan
+# def main():
+#     AlamatLengkap = "Mampang Prapatan, Jakarta Selatan, Indonesia"
+#     desa = ["Mampang Prapatan", "Tegal Parang", "Pela Mampang", "Bangka"]
+#     with ThreadPoolExecutor(max_workers=4) as executor:
+#         futures = [
+#             executor.submit(download_buildingMampang, f"{des}, {AlamatLengkap}")
+#             for des in desa
+#         ]
+#         results = []
+#         for done in as_completed(futures):
+#             results.append(done)
+
+
+def download_buildingJagakarsa(place):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        kec = place.replace(", Jagakarsa, Jakarta Selatan, Indonesia", "")
+        print(f"{kec} Start!")
+        try:
+            buildings = osm.features_from_place(
+                place, {"building": True, "type": "polygon"}
+            )
+            print(f"Successfully downloaded {kec}")
+        except Exception as e:
+            print(f"Failed to download {kec}: {e}")
+            return
+
+        # Ensure directory exists
+        output_dir = r"C:\Users\Lenovo\OneDrive - UGM 365\Documents\Second Brain\1 Projects\Skripsi\Code\Residential-Classification\Prepare the Dataset\building"  # Adjusted to use forward slashes
+        os.makedirs(
+            output_dir, exist_ok=True
+        )  # Create the directory if it doesn't exist
+
+        # Sanitize file name
+        file_path = os.path.join(output_dir, f"building_desa_{kec}.geojson")
+
+        try:
+            try:
+                buildings = (
+                    buildings[
+                        (buildings.geom_type == "Polygon")
+                        | (buildings.geom_type == "MultiPolygon")
+                    ]
+                    .reset_index(drop=True)
+                    .drop(columns="ways")
+                )
+            except:
+                pass
+            for column in buildings.columns:
+                if isinstance(buildings[column].iloc[0], list):
+                    buildings[column] = buildings[column].apply(
+                        lambda x: ", ".join(map(str, x)) if isinstance(x, list) else x
+                    )
+
+            buildings.to_file(file_path, driver="GeoJSON")
+            print(f"Successfully exported to {file_path}")
+        except Exception as e:
+            print(f"Failed to export {kec}: {e}")
+
+
+# def main(): # Jagakarsa
+#     AlamatLengkap = "Jagakarsa, Jakarta Selatan, Indonesia"
+#     desa = [
+#         "Jagakarsa",
+#         "Ciganjur",
+#         "Lenteng Agung",
+#         "Tanjung Barat",
+#         "Srengseng Sawah",
+#         "Cipedak",
+#     ]
+#     with ThreadPoolExecutor(max_workers=4) as executor:
+#         futures = [
+#             executor.submit(download_buildingJagakarsa, f"{des}, {AlamatLengkap}")
+#             for des in desa
+#         ]
+
+
+def download_buildingPesanggrahan(place):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        kec = place.replace(", Pesanggrahan, Jakarta Selatan, Indonesia", "")
+        print(f"{kec} Start!")
+        try:
+            buildings = osm.features_from_place(
+                place, {"building": True, "type": "polygon"}
+            )
+            print(f"Successfully downloaded {kec}")
+        except Exception as e:
+            print(f"Failed to download {kec}: {e}")
+            return
+
+        # Ensure directory exists
+        output_dir = r"C:\Users\Lenovo\OneDrive - UGM 365\Documents\Second Brain\1 Projects\Skripsi\Code\Residential-Classification\Prepare the Dataset\building"  # Adjusted to use forward slashes
+        os.makedirs(
+            output_dir, exist_ok=True
+        )  # Create the directory if it doesn't exist
+
+        # Sanitize file name
+        file_path = os.path.join(output_dir, f"building_desa_{kec}.geojson")
+
+        try:
+            try:
+                buildings = (
+                    buildings[
+                        (buildings.geom_type == "Polygon")
+                        | (buildings.geom_type == "MultiPolygon")
+                    ].reset_index(drop=True)
+                    # .drop(columns="ways")
+                )
+            except:
+                pass
+            for column in buildings.columns:
+                if isinstance(buildings[column].iloc[0], list):
+                    buildings[column] = buildings[column].apply(
+                        lambda x: ", ".join(map(str, x)) if isinstance(x, list) else x
+                    )
+
+            buildings.to_file(file_path, driver="GeoJSON")
+            print(f"Successfully exported to {file_path}")
+        except Exception as e:
+            print(f"Failed to export {kec}: {e}")
+
+
+def main_pesanggrahan():  # Pesanggrahan
+    AlamatLengkap = "Pesanggrahan, Jakarta Selatan, Indonesia"
+    desa = [
+        "Ulujami",
+        "Pesanggrahan",
+        "Petukangan Utara",
+        "Petukangan Selatan",
+        "Bintaro",
+    ]
     with ThreadPoolExecutor(max_workers=4) as executor:
         futures = [
-            executor.submit(download_building, f"{kec}, {city}") for kec in kecamatan
+            executor.submit(download_buildingPesanggrahan, f"{des}, {AlamatLengkap}")
+            for des in desa
         ]
-        results = []
-        for done in as_completed(futures):
-            results.append(done)
 
 
 if __name__ == "__main__":
-    main()
+    main_pesanggrahan()

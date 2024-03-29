eb56a7c867c370af3fa55f759d2f7a13c6813ad9
Zhulfaliant01 <zhulfaliant@gmail.com>
2024-03-29 13:55:09 +0700

    add merge_landuse.py

---
+++ Data Collection/merge_landuse.py
@@ -0,0 +1,18 @@
+import geopandas as gpd
+import pandas as pd
+import glob
+import os
+
+folder_path = r"C:\Users\Lenovo\OneDrive - UGM 365\Documents\Second Brain\1 Projects\Skripsi\Code\Residential-Classification\Prepare the Dataset\landuse"
+os.chdir(folder_path)
+files = glob.glob("*.shp")
+gdf_list = []
+for file in files:
+    print(file)
+    df = gpd.read_file(file)
+    gdf_list.append(df)
+
+gdf = gpd.GeoDataFrame(pd.concat(gdf_list))
+
+path_file = os.path.join(folder_path, "landuse_jaksel.geojson")
+gdf.to_file(path_file, driver="GeoJSON")

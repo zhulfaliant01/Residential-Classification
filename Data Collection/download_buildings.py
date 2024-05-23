5cd59d9ea96f4ec02fac3a781372d7598d0afd03
Zhulfaliant01 <zhulfaliant@gmail.com>
2024-05-23 13:43:59 +0700

    update download building and streets script

--- Data Collection/download_buildings.py
+++ Data Collection/download_buildings.py
@@ -6,58 +6,35 @@ import os
 
 warnings.catch_warnings(action="ignore")
 
-city = "Jakarta Selatan, Indonesia"
-kecamatan = [
-    "Cilandak",
-    "Kebayoran Baru",
-    "Kebayoran Lama",
-    "Pancoran",
-    "Pasar Minggu",
-    "Setiabudi",
-    "Tebet",
-]
+city = "Jakarta Utara, Indonesia"
+kecamatan = ["Cilincing"]
 
 
 def download_building(place):
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        kec = place.replace(", Jakarta Selatan, Indonesia", "")
+        kec = place.replace(", Jakarta Utara, Indonesia", "")
         print(f"{kec} Start!")
+        osm.config(timeout=300)
         try:
-            buildings = osm.features_from_place(
-                place, {"building": True, "type": "polygon"}
-            )
+            buildings = osm.features_from_place(place, {"building": True, "type": "polygon"})
             print(f"Successfully downloaded {kec}")
         except Exception as e:
             print(f"Failed to download {kec}: {e}")
 
         # Ensure directory exists
-        output_dir = r"C:\Users\Lenovo\OneDrive - UGM 365\Documents\Second Brain\1 Projects\Skripsi\Code\Residential-Classification\Prepare the Dataset\building"  # Adjusted to use forward slashes
-        os.makedirs(
-            output_dir, exist_ok=True
-        )  # Create the directory if it doesn't exist
+        output_dir = r"Data Collection\building"  # Adjusted to use forward slashes
+        os.makedirs(output_dir, exist_ok=True)  # Create the directory if it doesn't exist
 
         # Sanitize file name
-        file_name = place.replace(", Jakarta Selatan, Indonesia", "")
+        file_name = kec
         file_path = os.path.join(output_dir, f"building_{file_name}.geojson")
 
         try:
             buildings = buildings[
-                (buildings.geom_type == "Polygon")
-                | (buildings.geom_type == "MultiPolygon")
+                (buildings.geom_type == "Polygon") | (buildings.geom_type == "MultiPolygon")
             ].reset_index(drop=True)
-            buildings = buildings[
-                [
-                    "name",
-                    "building:levels",
-                    "building:roof",
-                    "building:walls",
-                    "building",
-                    "building:structure",
-                    "amenity",
-                    "geometry",
-                ]
-            ]
+            buildings = buildings[["building", "geometry"]]
             for column in buildings.columns:
                 for index, row in buildings.iterrows():
                     if isinstance(row[column], list):
@@ -71,10 +48,8 @@ def download_building(place):
 
 # Main function
 def main():
-    with ThreadPoolExecutor(max_workers=4) as executor:
-        futures = [
-            executor.submit(download_building, f"{kec}, {city}") for kec in kecamatan
-        ]
+    with ThreadPoolExecutor(max_workers=1) as executor:
+        futures = [executor.submit(download_building, f"{kec}, {city}") for kec in kecamatan]
         results = []
         for done in as_completed(futures):
             results.append(done)
@@ -86,9 +61,7 @@ def download_buildingMampang(place):
         kec = place.replace(", Mampang Prapatan, Jakarta Selatan, Indonesia", "")
         print(f"{kec} Start!")
         try:
-            buildings = osm.features_from_place(
-                place, {"building": True, "type": "polygon"}
-            )
+            buildings = osm.features_from_place(place, {"building": True, "type": "polygon"})
             print(f"Successfully downloaded {kec}")
         except Exception as e:
             print(f"Failed to download {kec}: {e}")
@@ -96,9 +69,7 @@ def download_buildingMampang(place):
 
         # Ensure directory exists
         output_dir = r"C:\Users\Lenovo\OneDrive - UGM 365\Documents\Second Brain\1 Projects\Skripsi\Code\Residential-Classification\Prepare the Dataset\building"  # Adjusted to use forward slashes
-        os.makedirs(
-            output_dir, exist_ok=True
-        )  # Create the directory if it doesn't exist
+        os.makedirs(output_dir, exist_ok=True)  # Create the directory if it doesn't exist
 
         # Sanitize file name
         file_name = place.replace(", Mampang Prapatan, Jakarta Selatan, Indonesia", "")
@@ -106,8 +77,7 @@ def download_buildingMampang(place):
 
         try:
             buildings = buildings[
-                (buildings.geom_type == "Polygon")
-                | (buildings.geom_type == "MultiPolygon")
+                (buildings.geom_type == "Polygon") | (buildings.geom_type == "MultiPolygon")
             ].reset_index(drop=True)
             buildings = buildings[
                 [
@@ -152,9 +122,7 @@ def download_buildingJagakarsa(place):
         kec = place.replace(", Jagakarsa, Jakarta Selatan, Indonesia", "")
         print(f"{kec} Start!")
         try:
-            buildings = osm.features_from_place(
-                place, {"building": True, "type": "polygon"}
-            )
+            buildings = osm.features_from_place(place, {"building": True, "type": "polygon"})
             print(f"Successfully downloaded {kec}")
         except Exception as e:
             print(f"Failed to download {kec}: {e}")
@@ -162,17 +130,14 @@ def download_buildingJagakarsa(place):
 
         # Ensure directory exists
         output_dir = r"C:\Users\Lenovo\OneDrive - UGM 365\Documents\Second Brain\1 Projects\Skripsi\Code\Residential-Classification\Prepare the Dataset\building"  # Adjusted to use forward slashes
-        os.makedirs(
-            output_dir, exist_ok=True
-        )  # Create the directory if it doesn't exist
+        os.makedirs(output_dir, exist_ok=True)  # Create the directory if it doesn't exist
 
         # Sanitize file name
         file_path = os.path.join(output_dir, f"building_desa_{kec}.geojson")
 
         try:
             buildings = buildings[
-                (buildings.geom_type == "Polygon")
-                | (buildings.geom_type == "MultiPolygon")
+                (buildings.geom_type == "Polygon") | (buildings.geom_type == "MultiPolygon")
             ].reset_index(drop=True)
             buildings = buildings[
                 [
@@ -197,21 +162,21 @@ def download_buildingJagakarsa(place):
             print(f"Failed to export {kec}: {e}")
 
 
-def main():  # Jagakarsa
-    AlamatLengkap = "Jagakarsa, Jakarta Selatan, Indonesia"
-    desa = [
-        "Jagakarsa",
-        "Ciganjur",
-        "Lenteng Agung",
-        "Tanjung Barat",
-        "Srengseng Sawah",
-        "Cipedak",
-    ]
-    with ThreadPoolExecutor(max_workers=4) as executor:
-        futures = [
-            executor.submit(download_buildingJagakarsa, f"{des}, {AlamatLengkap}")
-            for des in desa
-        ]
+# def main():  # Jagakarsa
+#     AlamatLengkap = "Jagakarsa, Jakarta Selatan, Indonesia"
+#     desa = [
+#         "Jagakarsa",
+#         "Ciganjur",
+#         "Lenteng Agung",
+#         "Tanjung Barat",
+#         "Srengseng Sawah",
+#         "Cipedak",
+#     ]
+#     with ThreadPoolExecutor(max_workers=4) as executor:
+#         futures = [
+#             executor.submit(download_buildingJagakarsa, f"{des}, {AlamatLengkap}")
+#             for des in desa
+#         ]
 
 
 def download_buildingPesanggrahan(place):
@@ -220,9 +185,7 @@ def download_buildingPesanggrahan(place):
         kec = place.replace(", Pesanggrahan, Jakarta Selatan, Indonesia", "")
         print(f"{kec} Start!")
         try:
-            buildings = osm.features_from_place(
-                place, {"building": True, "type": "polygon"}
-            )
+            buildings = osm.features_from_place(place, {"building": True, "type": "polygon"})
             print(f"Successfully downloaded {kec}")
         except Exception as e:
             print(f"Failed to download {kec}: {e}")
@@ -230,17 +193,14 @@ def download_buildingPesanggrahan(place):
 
         # Ensure directory exists
         output_dir = r"C:\Users\Lenovo\OneDrive - UGM 365\Documents\Second Brain\1 Projects\Skripsi\Code\Residential-Classification\Prepare the Dataset\building"  # Adjusted to use forward slashes
-        os.makedirs(
-            output_dir, exist_ok=True
-        )  # Create the directory if it doesn't exist
+        os.makedirs(output_dir, exist_ok=True)  # Create the directory if it doesn't exist
 
         # Sanitize file name
         file_path = os.path.join(output_dir, f"building_desa_{kec}.geojson")
 
         try:
             buildings = buildings[
-                (buildings.geom_type == "Polygon")
-                | (buildings.geom_type == "MultiPolygon")
+                (buildings.geom_type == "Polygon") | (buildings.geom_type == "MultiPolygon")
             ].reset_index(drop=True)
             buildings = buildings[
                 [
@@ -265,20 +225,20 @@ def download_buildingPesanggrahan(place):
             print(f"Failed to export {kec}: {e}")
 
 
-def main():  # Pesanggrahan
-    AlamatLengkap = "Pesanggrahan, Jakarta Selatan, Indonesia"
-    desa = [
-        "Ulujami",
-        "Pesanggrahan",
-        "Petukangan Utara",
-        "Petukangan Selatan",
-        "Bintaro",
-    ]
-    with ThreadPoolExecutor(max_workers=4) as executor:
-        futures = [
-            executor.submit(download_buildingPesanggrahan, f"{des}, {AlamatLengkap}")
-            for des in desa
-        ]
+# def main():  # Pesanggrahan
+#     AlamatLengkap = "Pesanggrahan, Jakarta Selatan, Indonesia"
+#     desa = [
+#         "Ulujami",
+#         "Pesanggrahan",
+#         "Petukangan Utara",
+#         "Petukangan Selatan",
+#         "Bintaro",
+#     ]
+#     with ThreadPoolExecutor(max_workers=4) as executor:
+#         futures = [
+#             executor.submit(download_buildingPesanggrahan, f"{des}, {AlamatLengkap}")
+#             for des in desa
+#         ]
 
 
 if __name__ == "__main__":
--- Data Collection/download_streets.py
+++ Data Collection/download_streets.py
@@ -1,29 +1,29 @@
 import geopandas as gpd
 import osmnx as osm
-from concurrent.futures import ThreadPoolExecutor, as_completed
 import warnings
 import os
 
 warnings.catch_warnings(action="ignore")
 
-graphs = osm.graph_from_place("Jakarta Selatan, Indonesia")
-output_dir = r"C:\Users\Lenovo\OneDrive - UGM 365\Documents\Second Brain\1 Projects\Skripsi\Code\Residential-Classification\Data Collection\street"
-file_path = os.path.join(output_dir, f"streets_jaksel_drive.geojson")
+for kec in ["Cilincing", "Kelapa Gading"]:
+    graphs = osm.graph_from_place(f"{kec}, Jakarta Utara, Indonesia")
+    output_dir = r"C:\Users\Lenovo\OneDrive - UGM 365\Documents\Second Brain\1 Projects\Skripsi\Code\Residential-Classification\Data Collection\street"
+    file_path = os.path.join(output_dir, f"streets_{kec}_drive.geojson")
 
-try:
-    streets = osm.graph_to_gdfs(
-        graphs,
-        nodes=False,
-        edges=True,
-        node_geometry=False,
-        fill_edge_geometry=True,
-    )
-    streets = streets[~streets.highway.isna()].reset_index(drop=True)
-    streets = streets[["geometry", "highway"]]
-    for column in streets.columns:
-        for index, row in streets.iterrows():
-            if isinstance(row[column], list):
-                streets.loc[index, column] = ""
-    streets.to_file(file_path, driver="GeoJSON")
-except Exception as e:
-    print(f"Failed to export {e}")
+    try:
+        streets = osm.graph_to_gdfs(
+            graphs,
+            nodes=False,
+            edges=True,
+            node_geometry=False,
+            fill_edge_geometry=True,
+        )
+        streets = streets[~streets.highway.isna()].reset_index(drop=True)
+        streets = streets[["geometry", "highway"]]
+        for column in streets.columns:
+            for index, row in streets.iterrows():
+                if isinstance(row[column], list):
+                    streets.loc[index, column] = ""
+        streets.to_file(file_path, driver="GeoJSON")
+    except Exception as e:
+        print(f"Failed to export {e}")

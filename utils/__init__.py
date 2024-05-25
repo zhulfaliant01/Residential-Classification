4227ce71989e72c33683f4c89b407854d8324bb3
Zhulfaliant01 <zhulfaliant@gmail.com>
2024-05-25 14:42:07 +0700

    Add utils

---
+++ utils/__init__.py
@@ -0,0 +1,2 @@
+# utils/__init__.py
+from .data_process import read_csv_to_wkt
---
+++ utils/data_process.py
@@ -0,0 +1,20 @@
+# utils/data_process.py
+import pandas as pd
+import geopandas as gpd
+from shapely import wkt
+
+
+def read_csv_to_wkt(file, geom_column="geometry"):
+    print("Reading CSV file...")
+    data = pd.read_csv(file, index_col=0)
+    print(f"Data loaded: {data.shape[0]} rows and {data.shape[1]} columns")
+
+    print("Applying WKT loads...")
+    data[geom_column] = data[geom_column].apply(wkt.loads)
+    print(f"WKT conversion done: {data[geom_column].notnull().sum()} geometries loaded")
+
+    print("Converting to GeoDataFrame...")
+    data = gpd.GeoDataFrame(data, geometry=geom_column)  # type: ignore
+    print("Conversion to GeoDataFrame done.")
+
+    return data

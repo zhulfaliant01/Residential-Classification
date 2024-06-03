cf4c9fb1aff5b41352f2b714ea7d1b3e589d547e
Zhulfaliant01 <zhulfaliant@gmail.com>
2024-06-03 14:50:55 +0700

    update utils init

--- Data Processing/utils/__init__.py
+++ Data Processing/utils/__init__.py
@@ -1,9 +1,3 @@
 # utils/__init__.py
-from .data_process import (
-    read_csv_to_wkt,
-    check_and_set_crs,
-    del_gc,
-    mm_std_character,
-    mm_total_area,
-)
+from .data_process import *
 from .topology_check import check_overlap, check_gap, check_containment

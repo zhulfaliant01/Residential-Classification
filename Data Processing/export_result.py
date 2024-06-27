287e53e72742c47c5ef5cbe2c24b5a799af29bd6
Zhulfaliant01 <zhulfaliant@gmail.com>
2024-06-27 23:58:25 +0700

    Add export results script

--- Data Processing/export_result.py
+++ Data Processing/export_result.py
@@ -1,4 +1,3 @@
-import geopandas as gpd
 import matplotlib.pyplot as plt
 from utils import read_csv_to_wkt
 import numpy as np
@@ -6,12 +5,18 @@ import os
 import glob
 import re
 
+datal = "leak"
+
 folder = r"Data Modelling\testing_results"
-files = glob.glob(os.path.join(folder, "*_noleak_9.csv"))
+files = (
+    glob.glob(os.path.join(folder, "*_noleak_9.csv"))
+    if datal == "noleak"
+    else glob.glob(os.path.join(folder, "*Exp_81.csv"))
+)
 # Read CSV file
 
 for file in files:
-    name = re.search(r"([\w ]*)_noleak", file).group(1)  # type: ignore
+    name = re.search(r"([\w ]*)_noleak", file).group(1) if datal == "noleak" else re.search(r"([\w ]*)Exp_", file).group(1)  # type: ignore
     df = read_csv_to_wkt(file)
     print(name)
 
@@ -23,18 +28,49 @@ for file in files:
             (labels == 0) & (preds == 1),
             (labels == 1) & (preds == 0),
         ]
-        choices = ["TP", "TN", "FP", "FN"]
+        choices = [
+            "Positif Benar",
+            "Negatif Benar",
+            "Positif Salah",
+            "Negatif Salah",
+        ]
         return np.select(conditions, choices)
+
+    colors = {
+        "Positif Benar": "#2ac1d1",
+        "Negatif Benar": "#e280b8",
+        "Positif Salah": "#d62a2b",
+        "Negatif Salah": "#0e6cae",
+    }
 
     # Apply the function to the dataframe
     df["conf_matrix"] = conf_matrix_vectorized(df["label"], df["pred_labels"])
     df["prob_loss"] = abs(df["pred_proba"] - df["label"])
     df["prob_loss"] = df["prob_loss"].apply(lambda x: x if x >= 0.518 else 0)
+    # Plot dengan warna khusus
 
-    # Create the plot
-    ax = df.plot("conf_matrix", legend=True, figsize=(10, 10))  # type: ignore
+    # Create a color map for plotting
+    df["color"] = df["conf_matrix"].map(colors)  # type: ignore
+
+    # Plot with colors
+    fig, ax = plt.subplots(1, 1, figsize=(25, 25))
+    df.plot(ax=ax, color=df["color"])
+
+    # Custom legend
+    import matplotlib.patches as mpatches
+
+    legend_handles = [
+        mpatches.Patch(color=color, label=label)
+        for label, color in colors.items()
+    ]
+    ax.legend(handles=legend_handles, loc="best")
     ax.set_axis_off()
 
     # Save the plot
-    folder_save = r"C:\Users\Lenovo\OneDrive - UGM 365\Documents\Second Brain\1 Projects\Skripsi\Dokumen\Hasil\No Local Data"
+    folder_save = (
+        r"C:\Users\Lenovo\OneDrive - UGM 365\Documents\Second Brain\1 Projects\Skripsi\Dokumen\Hasil\No Local Data"
+        if datal == "noleak"
+        else r"C:\Users\Lenovo\OneDrive - UGM 365\Documents\Second Brain\1 Projects\Skripsi\Dokumen\Hasil\10% Local Data"
+    )
+    print("saving...")
     plt.savefig(os.path.join(folder_save, f"{name}.png"))
